0e5a97bfebd78a948baacbd119858a5797145a5e
Seppe Lenders <seppe.lenders@gmail.com>
2024-04-24 22:02:14 +0200

    avoid thrashing

--- controller/src/controller/app.py
+++ controller/src/controller/app.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 import sys
 
-from . import config
+from . import config, motor
 from . import motor_io as _motor_io
 from . import roof
 from .scheduler import scheduler
@@ -30,8 +30,17 @@ def loop():
     scheduler.delay(loop, config.poll_duration)
 
     for roof in roofs.values():
-        for motor in roof.motors.values():
-            if motor.read():
-                roof.do_movement(motor.direction, 1)
+        motor_open = roof.motors[motor.Motor.Direction.OPEN]
+        motor_close = roof.motors[motor.Motor.Direction.CLOSE]
+
+        motor_open_input = motor_open.read()
+        motor_close_input = motor_close.read()
+
+        if motor_open_input and motor_close_input:
+            pass
+        elif motor_open_input:
+            roof.open(1)
+        elif motor_close_input:
+            roof.close(1)
 
 
